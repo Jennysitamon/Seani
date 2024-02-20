287bfdca2a8c93f722f279c651bbac6e0aa8dd24
jeny <jenymontiel13@gmail.com>
2024-02-20 14:03:14 -0600

    02 App Library

--- seani/seani/settings.py
+++ seani/seani/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Local Apps
+    'library.apps.LibraryConfig',
+    # Django Apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,7 +79,7 @@ WSGI_APPLICATION = 'seani.wsgi.application'
 DATABASES = {
     'default': {
             'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'seani',
+            'NAME': 'seani2024',
             'USER': 'root',
             'PASSWORD': '',
             'HOST': 'localhost',
@@ -125,3 +128,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
